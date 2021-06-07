c96997e9f79f55a2714246408f172fae35d36e39
Jules VERON <juveron@e2r8p14.42.fr>
2021-06-07 15:24:23 +0200

    update src and walktrougt

--- level0/src.c
+++ level0/src.c
@@ -1,1 +1,13 @@
-insert the code of the binary
+#include<unistd.h>
+#include<stdio.h>
+int		main(int argc, char **argv)
+{
+	if (atoi(argv[1]) == 423)
+	{
+		execv("/bin/bash", "bash"
+
+	}
+	else
+		printf("No !")
+	return 0;
+}
