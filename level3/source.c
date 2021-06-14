d203998cb1ac5ffb2be3fda4265c27521d0c701c
Jules VERON <juveron@e2r8p14.42.fr>
2021-06-14 14:13:28 +0200

    update source.c

---
+++ level3/source.c
@@ -0,0 +1,19 @@
+int		v(void)
+{
+	int size = 512;
+	char *s;
+	char *format
+	FILE *stream
+	fgets(format, size, stdin);
+	printf(buffer);
+	if (s[size] == 64)
+	{
+		fwrite("Wait what?!\n", 12, 1, stdout);
+		system("/bin/sh");
+	}
+	return (0);
+}
+int		main(void)
+{
+	v();
+	return (0);
